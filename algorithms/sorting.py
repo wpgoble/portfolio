41a21e3d9df36cfab7302b9e848136f876239b1c
William Goble <will.goble@gmail.com>
2023-01-28 14:31:50 -0500

    adds merge sort

--- algorithms/sorting.py
+++ algorithms/sorting.py
@@ -20,10 +20,41 @@ def quicksort(lst):
     return quicksort(left) + middle + quicksort(right)
 
 
+def mergesort(lst):
+    """
+    Merge sort is a divide-and-conquer algorithm that divides an array in
+    two, sorts the two halves, and then merges them back together.
+    """
+    if len(lst) <= 1:
+        return lst
+
+    mid = len(lst) // 2
+    left = mergesort(lst[:mid])
+    right = mergesort(lst[mid:])
+    return merge(left, right)
+
+
+def merge(left, right):
+    result = list()
+    i = 0
+    j = 0
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+    result += left[i:]
+    result += right[j:]
+    return result
+
+
 def main():
     temp = [3, 6, 8, 10, 1, 2, 1]
     print(f"Original:\t{temp}")
     print(f"Quick Sort:\t{quicksort(temp)}")
+    print(f"Merge Sort:\t{mergesort(temp)}")
 
 
 if __name__ == "__main__":
